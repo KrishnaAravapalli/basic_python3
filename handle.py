e6fa5ccc1f235c6844f91c9655d86eaf053ae738
KrishnaAravapalli <krishnaravapalli334@gail.com>
2025-03-07 20:52:32 +0530

    Initial commit

---
+++ handle.py
@@ -0,0 +1,9 @@
+try:
+   
+    with open("sample.txt", "r") as file:
+       
+        for line in file:
+            print(line.strip()) 
+
+except FileNotFoundError:
+    print("Error: The file 'sample.txt' not found.")
